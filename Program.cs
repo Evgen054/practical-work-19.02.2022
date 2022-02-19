fa1e98e8f8c72ccf5bdc4698c4f328f25ccb0936
EVG54 <bankir032@mail.ru>
2022-02-20 01:38:35 +0300

    create the PrintStringArray method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
+ либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+ лучше обойтись исключительно массивами. */
+using static System.Console;
+
+void PrintStringArray(string[] stringArray)
+{
+    Write("[");
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Write($"\"{stringArray[i]}\"");
+        if (i != stringArray.Length - 1)
+        {
+            Write(", ");
+        }
+    }
+    Write("]");
+}
+string[] stringArray = { "hello", "2", "world", ":-)" };
+
+PrintStringArray(stringArray);
+
+
